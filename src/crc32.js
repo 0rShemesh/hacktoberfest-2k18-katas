72cf70515bdc672c6c5dbfd56c9f2bd31d0b681c
0rShemesh <34350066+0rShemesh@users.noreply.github.com>
2018-10-15 12:32:22 +0300

    implemented the crc32
    Hello, I think it implemented right!
    if do u want to check the return of crc32 function when the input is "hello" equal to 0x3610A686

--- src/crc32.js
+++ src/crc32.js
@@ -1,3 +1,28 @@
-export const crc32 = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+var makeCRC32Table = function(){
+    var c;
+    var crc32Table = [];
+    for(var n =0; n < 256; n++){
+        c = n;
+        for(var k =0; k < 8; k++){
+            if(c&1)
+            {
+              c= (0xEDB88320 ^ (c >>> 1));
+            }else{
+                  c= (c>>>1);
+            };
+        }
+        crc32Table[n] = c;
+    }
+    return crc32Table;
+}
+
+var crc32 = function(str) {
+    var crcTable = window.crcTable || (window.crcTable = makeCRC32Table());
+    var crc = 0 ^ (-1);
+
+    for (var i = 0; i < str.length; i++ ) {
+        crc = (crc >>> 8) ^ crcTable[(crc ^ str.charCodeAt(i)) & 0xFF];
+    }
+
+    return (crc ^ (-1)) >>> 0;
 };
